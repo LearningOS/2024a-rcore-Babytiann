66dbe6853e2f1a8b22b1c6dc5fd7c192dae00a37
babytian <2413915435@qq.com>
2024-10-26 14:55:34 +0800

    done

--- os/src/sbi.rs
+++ os/src/sbi.rs
@@ -4,7 +4,7 @@ use core::arch::asm;
 
 const SBI_SET_TIMER: usize = 0;
 const SBI_CONSOLE_PUTCHAR: usize = 1;
-const SBI_SHUTDOWN: usize = 8;
+const SBI_RESET: usize = 0x53525354;
 
 /// general sbi call
 #[inline(always)]
@@ -12,12 +12,12 @@ fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
     let mut ret;
     unsafe {
         asm!(
-            "ecall",
-            inlateout("x10") arg0 => ret,
-            in("x11") arg1,
-            in("x12") arg2,
-            in("x16") 0,
-            in("x17") which,
+        "ecall",
+        inlateout("x10") arg0 => ret,
+        in("x11") arg1,
+        in("x12") arg2,
+        in("x16") 0,
+        in("x17") which,
         );
     }
     ret
@@ -35,6 +35,6 @@ pub fn console_putchar(c: usize) {
 
 /// use sbi call to shutdown the kernel
 pub fn shutdown() -> ! {
-    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
+    sbi_call(SBI_RESET, 0, 0, 0);
     panic!("It should shutdown!");
 }
